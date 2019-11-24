e3d631ae2a01418b04d5421217eadcd678248656
DjArt <art.letter.mail@gmail.com>
2019-11-24 22:53:57 +0700

    Filter added

---
+++ EmptyBox.Automation/Filter.fs
@@ -0,0 +1,19 @@
+﻿namespace EmptyBox.Automation
+
+open System
+
+type public Filter<'TData>(predicate : Predicate<'TData>) =
+    inherit Pipeline<'TData, 'TData>()
+
+    let InternalEvent = Event<EventHandler<'TData>, 'TData>()
+
+    interface IPipelineIO<'TData, 'TData> with
+        member this.Input = EventHandler<'TData>(this.Input)
+        [<CLIEvent>]
+        member this.Output = InternalEvent.Publish
+
+    member private this.Input _ x =
+        match predicate.Invoke x with
+        | true ->
+            InternalEvent.Trigger(this, x)
+        | _ -> ()
